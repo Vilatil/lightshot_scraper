6aa03a89a85f8968ed31bd118a646b9467c2c4b5
Vilatil <hestegtocka@gmail.com>
2025-01-24 16:09:55 +0200

    feat: 24.I.2025, better logging, bug fixes

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ headers = {
 URL="https://prnt.sc/"
 
 def main():
-    logging.basicConfig(level=logging.WARNING)
+    logging.basicConfig(level=logging.INFO)
     create_a_folder()
     while True:
-        proper_link = f'{URL}{create_a_link(5)}'
-        get_a_link(proper_link)
+        time.sleep(3)
+        created_link = URL + create_a_link(5)
+        logging.info(f"the created link: {created_link}")
+        get_src(created_link)
     return
 
 def create_a_folder():
@@ -25,28 +27,22 @@ def create_a_folder():
     if not os.path.exists(folder):
         os.makedirs(folder)
 def create_a_link(k):
-    link=""
-    while k >= 0:
-        letter_or_number = random.randint(0,1)
-        if (letter_or_number == 0):
-           number = random.choice(string.digits)
-           link+=number
-        else:
-            letter = random.choice(string.ascii_lowercase)
-            link+=letter
-        k-=1
+    characters = string.ascii_lowercase + string.digits
+    link = ''.join(random.choices(characters, k=k))
     return link
 
-def get_a_link(proper_link):
-    response = requests.get(proper_link, headers=headers)
+def get_src(created_link):
+    response = requests.get(created_link, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
     picture = soup.find('img', class_='no-click screenshot-image')
     if not picture:
         logging.warning("picture not found, gonna try another link")
-        create_a_link(5)
         return
     light_shot_link = picture.get('src')
-    logging.info(f"{light_shot_link}")
+    if "0_173a7b_211be8ff.png" in light_shot_link:
+        logging.info("the photo was removed from the website")
+        return
+    logging.info(f"the link for downloading: {light_shot_link}")
     downloader(light_shot_link)
 
 def downloader(light_shot_link):
@@ -54,12 +50,12 @@ def downloader(light_shot_link):
         light_shot_link = "https:" + light_shot_link
     response = requests.get(light_shot_link, headers=headers)
     if response.status_code != 200:
-        logging.error(f"Failed to download image!{response.status_code}")
+        logging.error(f"Failed to download image! ERROR:{response.status_code}")
         return
     filename = light_shot_link.split('/')[-1]
     with open(f'./images/{filename}', 'wb') as img:
         img.write(response.content)
-    logging.info("Image downloaded successfully!")
+    logging.info("Image was successfully downloaded!")
 
 if __name__ == "__main__":
     main()
